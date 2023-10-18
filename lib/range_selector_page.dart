6a260ec6391049a53d9ae10a73d87e2df14782e4
vivienphang <vivienphang@gmail.com>
2023-10-18 23:39:15 +0800

    added validation for input

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -8,13 +8,18 @@ class RangeSelectorPage extends StatefulWidget {
 }
 
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  final formKey = GlobalKey<FormState>();
+  int _min = 0;
+  int _max = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
-      body: const Form(
+      body: Form(
+        key: formKey,
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
@@ -22,43 +27,67 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
               children: [
                 FormFieldWidget(
                   labelText: 'Minimum',
+                  intValueSetter: (value) => _min = value,
                 ),
-                SizedBox(height: 12),
+                SizedBox(
+                    height: 12
+                ),
                 FormFieldWidget(
-                    labelText: 'Maximum')
+                    labelText: 'Maximum',
+                  intValueSetter: (value) => _max = value,
+                ),
               ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
-        onPressed: () {},
+        onPressed: () {
+          // TODO: Validate the form
+          if (formKey.currentState?.validate() == true) {
+            formKey.currentState?.save();
+
+          }
+          // TODO: Navigate to the generator page
+        },
       ),
-      // TODO: Validate the form
-      // TODO: Navigate to the generator page
+
     );
   }
 }
 
 class FormFieldWidget extends StatelessWidget {
-  final String labelText;
-  // Creating widget with optional key and mandatory labelText
+  // Creating custom widget with optional key and mandatory labelText
   const FormFieldWidget({
     Key? key,
     required this.labelText,
+    required this.intValueSetter,
   }) : super(key: key);
+
+  final String labelText;
+  // Create a callback function that takes in int as param
+  final void Function(int value) intValueSetter;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
     decoration: InputDecoration(
-      border: OutlineInputBorder(),
+      border: const OutlineInputBorder(),
       labelText: labelText,
     ),
-    keyboardType: TextInputType.numberWithOptions(
+    keyboardType: const TextInputType.numberWithOptions(
       decimal: false,
       signed: true,
     ),
-              );
+      validator: (value) {
+      // If the text provided is not a string, return error message
+        if (value == null || int.tryParse(value) == null) {
+          return 'Must enter integer values.';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
+    );
   }
 }
