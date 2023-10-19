636fe0da5b21fb6f518e0e82fe3f928e090f6b5e
vivienphang <vivienphang@gmail.com>
2023-10-19 16:52:23 +0800

    part one done

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,5 +1,4 @@
-
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -9,20 +8,39 @@ class RandomizerPage extends StatefulWidget {
     Key? key,
     required this.min,
     required this.max,
-    }) : super(key: key);
+  }) : super(key: key);
 
   @override
   _RandomizerPageState createState() => _RandomizerPageState();
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+  final randomGenerator = Random();
+
+
   @override
   Widget build(BuildContext context) {
     widget.min;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Randomizer')
+          title: const Text('Randomizer')
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? 'Generate a number',
+          style: const TextStyle(fontSize: 42),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
